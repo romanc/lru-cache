ec81a6305f2191da8ae46934283373d79096fe2d
Roman Cattaneo <>
2022-01-07 22:29:41 +0100

    Remove debug stuff

--- LRUCache.h
+++ LRUCache.h
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <iostream>
 #include <list>
 #include <map>
 #include <string>
@@ -13,7 +12,6 @@ public:
 
     // known key, remove from recentlyUsed
     if (search != data.end()) {
-      std::cout << "found existing key" << std::endl;
       auto start = std::remove(recentlyUsed.begin(), recentlyUsed.end(), key);
       recentlyUsed.erase(start, recentlyUsed.end());
     }
@@ -22,19 +20,8 @@ public:
     data[key] = value;
 
     if (data.size() > capacity) {
-      std::cout << "removing last recently used element" << std::endl;
-      std::cout << "data.size() = " << data.size() << std::endl;
-      std::cout << "rUsed.size() = " << recentlyUsed.size() << std::endl;
-
-      auto remove = recentlyUsed.back();
-
-      // std::cout << remove << std::endl;
-      std::cout << "removing " << remove << " from cache" << std::endl;
-      data.erase(remove);
+      data.erase(recentlyUsed.back());
       recentlyUsed.pop_back();
-
-      std::cout << "data.size() = " << data.size() << std::endl;
-      std::cout << "rUsed.size() = " << recentlyUsed.size() << std::endl;
     }
   }
 
@@ -51,20 +38,6 @@ public:
 
     // return the value associated with this key
     return search->second;
-  }
-
-  void debug() const {
-    std::cout << "keys: ";
-    for (const auto &[key, value] : data) {
-      std::cout << key << " ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "Last recently used: ";
-    for (const auto &key : recentlyUsed) {
-      std::cout << key << " ";
-    }
-    std::cout << std::endl;
   }
 
 private:
--- main.cpp
+++ main.cpp
@@ -9,30 +9,24 @@ int main() {
 
   cout << "put(A)" << endl;
   cache.put("A", "alpha");
-  cache.debug();
 
   cout << "put(B)" << endl;
   cache.put("B", "beta");
-  cache.debug();
 
   cout << "put(B)" << endl;
   cache.put("B", "beta");
-  cache.debug();
 
   cout << "put(C)" << endl;
   cache.put("C", "gamma");
-  cache.debug();
 
   cout << "put(D)" << endl;
   cache.put("D", "delta");
-  cache.debug();
 
   cout << "cache[A]: " << cache.get("A") << endl;
   cout << "cache[A]: " << cache.get("A") << endl;
   cout << "cache[B]: " << cache.get("B") << endl;
   cout << "cache[C]: " << cache.get("C") << endl;
-
-  cache.debug();
+  cout << "cache[C]: " << cache.get("C") << endl;
 
   return 0;
 }
